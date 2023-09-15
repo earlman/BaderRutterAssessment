edd664b8774b9dba93a1647068d73bd8c87a0308
earlman <earl.mangulabnan@gmail.com>
2023-09-14 19:17:54 -0500

    set up store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
+import router from "./router";
 
-import "./assets/main.css";
-
-app = new Vue({
+const app = new Vue({
    router,
    store,
+   render: (h) => h(App),
+   beforeMount() {
+      // if (user) {
+      //    // set user in Vuex if they exist
+      //    store.commit("SET_USER", user);
+      //    const p1 = store.dispatch("fetchUserConfig").then(() => {
+      //       console.log("main.js: user config loaded");
+      //    });
+      //    const p2 = store.dispatch("fetchAppConfig").then(() => {
+      //       console.log("main.js: app config loaded");
+      //    });
+      //    Promise.all([p1, p2]).then(() => {
+      //       this.$forceUpdate();
+      //    });
+      // }
+   },
 }).$mount("#app");
